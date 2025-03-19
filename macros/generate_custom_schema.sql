85e4e044037e8d8ba47a79db03438ea28755b3b0
Hareesh Suresh <hareeshs@systechusa.com>
2025-03-19 10:11:36 +0000

    commit

--- macros/generate_custom_schema.sql
+++ macros/generate_custom_schema.sql
@@ -12,3 +12,12 @@
     {%- endif -%}
 
 {%- endmacro %}
+
+{% macro log_model_run_time() %}
+    {% for result in run_results.results %}
+        {% set model_name = result.unique_id %}
+        {% set start_time = result.timing[0].started_at %}
+        {% set end_time = result.timing[0].completed_at %}
+        {{ log("Model: " ~ model_name ~ ", Start: " ~ start_time ~ ", End: " ~ end_time, info=True) }}
+    {% endfor %}
+{% endmacro %}
