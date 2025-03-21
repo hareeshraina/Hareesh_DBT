85c3392fcc2131167674024a27ba38976ebff1fa
Hareesh Suresh <hareeshs@systechusa.com>
2025-03-21 12:05:55 +0000

    cucfyf

--- models/DBT_Workshop/Prior_year_sales.sql
+++ models/DBT_Workshop/Prior_year_sales.sql
@@ -1,14 +1,1 @@
-with source as
-(
-    select productprice,purchaseprice,customerid from RETAIL_DATA
-),
-prior_sales as
-(
- select customerid,productprice  from source --where datatrunc()
-),
-current_sales as 
-(
-
-)
-
-select * from {{ source('DBT_Workshop', 'RETAIL_DATA') }}
+{{created_by()}}
